30cae6068238615f0652fe4e2105f859914c5501
hieutt03 <hieuttjob@gmail.com>
2024-07-14 03:21:59 +0700

    change aspect

--- src/config/products.ts
+++ src/config/products.ts
@@ -10,3 +10,32 @@ export const PRODUCT_PRICES = {
 } as const
 
 export const BASE_PRICE = 140_000
+
+export const ASPECT_PHONE= [
+  {
+    name: "iphone11",
+    height: 1831,
+    width: 896
+  },
+  {
+    name: "iphone12",
+    height: 2778,
+    width: 1284
+  },
+  {
+    name: "iphone13",
+    height: 2778,
+    width: 1284
+  },
+  {
+    name: "iphone14",
+    height: 2796,
+    width: 1290
+  },
+  {
+    name: "iphone15",
+    height: 2796,
+    width: 1290
+  }
+  
+] 
